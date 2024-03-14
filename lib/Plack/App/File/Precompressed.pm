7919fb1e0129b0954a0332c2d4682e4b2ba88176
Aristotle Pagaltzis <pagaltzis@gmx.de>
2024-03-14 12:27:35 +0100

    initial checkin

---
+++ lib/Plack/App/File/Precompressed.pm
@@ -0,0 +1,27 @@
+package Plack::App::File::Precompressed;
+our $VERSION = '1.001';
+use parent 'Plack::Middleware::Static::Precompressed';
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Plack::App::File::Precompressed - serve a tree of static pre-compressed files
+
+=head1 SYNOPSIS
+
+ my $asset_app = Plack::App::File::Precompressed->new( root => 'static' )->to_app;
+
+=head1 DESCRIPTION
+
+This is an empty wrapper around L<Plack::Middleware::Static::Precompressed>,
+which already works as a L<PSGI> application.
+The wrapper exists only in case you find it weird to instantiate and use
+a Plack::Middleware as an application; you do not need to use it.
+
+=cut
